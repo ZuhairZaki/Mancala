2beebbbf9399be6c8e8f1a1bbe6d304950fa370c
S. M. Zuhair Zaki <zuhairzaki500@gmail.com>
2021-12-16 07:29:36 -0500

    Add files via upload

--- Heuristics.py
+++ Heuristics.py
@@ -1,3 +1,4 @@
+from typing import Sized
 from MancalaBoard import *
 
 def beans_difference(board: BoardState):
@@ -29,6 +30,25 @@ def repeat_turns(max_side):
             max_chain = max(max_chain,repeat_turns(side_copy)+1)
     return max_chain
 
+def maximize_capture(max_side,min_side):
+    max_cap = 0
+    side_copy1 = max_side[:]
+    side_copy2 = min_side[:]
+    no_pockets = len(side_copy1)
+    for i in range(no_pockets):
+        beans_amount = side_copy1[i]
+        if beans_amount!=0:
+            j = i+beans_amount
+            if j<no_pockets:
+                if side_copy1[j]==0:
+                    max_cap = max(max_cap,side_copy2[no_pockets-j-1]+1)
+            else:
+                j -= (2*no_pockets+1)
+                if j>=0 and j<=i:
+                    if j==i or side_copy1[j]==0:
+                        max_cap = max(max_cap,side_copy2[no_pockets-j-1]+2)
+    return max_cap
+ 
 def heuristic1(board: BoardState):
     w = 1
     return w*beans_difference(board)
@@ -44,8 +64,39 @@ def heuristic3(board: BoardState):
     w3 = 0.5
     return w1*beans_difference(board) + w2*beans_hoarded(board) + w3*repeat_turns(board.player_side[0])
 
+def heuristic4(board: BoardState):
+    w1 = 1
+    w2 = 0.5
+    w3 = 0.7
+    w4 = -0.3
+    return w1*beans_difference(board) + w2*beans_hoarded(board) + w3*repeat_turns(board.player_side[0]) \
+    + w4*repeat_turns(board.player_side[1])
+
+def heuristic5(board: BoardState):
+    w1 = 1
+    w2 = 0.5
+    w3 = 0.7
+    w4 = -0.3
+    w5 = 0.4
+    return w1*beans_difference(board) + w2*beans_hoarded(board) + w3*repeat_turns(board.player_side[0]) \
+    + w4*repeat_turns(board.player_side[1]) + w5*maximize_capture(board.player_side[0],board.player_side[1]) 
+
+def heuristic6(board: BoardState):
+    w1 = 1
+    w2 = 0.5
+    w3 = 0.7
+    w4 = -0.3
+    w5 = 0.4
+    w6 = -0.1
+    return w1*beans_difference(board) + w2*beans_hoarded(board) + w3*repeat_turns(board.player_side[0]) \
+    + w4*repeat_turns(board.player_side[1]) + w5*maximize_capture(board.player_side[0],board.player_side[1]) \
+    + w6*maximize_capture(board.player_side[1],board.player_side[0])
+
 heu_dict = { 
     "heuristic1": heuristic1,
     "heuristic2": heurisric2,
-    "heuristic3": heuristic3
+    "heuristic3": heuristic3,
+    "heuristic4": heuristic4,
+    "heuristic5": heuristic5,
+    "heuristic6": heuristic6
 }
--- main.py
+++ main.py
@@ -12,30 +12,62 @@ choice = int(input())
 
 if choice==1:
     curr_board = BoardState()
+    print(curr_board)
     while not curr_board.is_terminal():
-        print(curr_board)
         if curr_board.player==0:
-            val, ai_move = alpha_beta_decision(curr_board,-math.inf,math.inf,11,"heuristic3")
+            val, ai_move = alpha_beta_decision(curr_board,-math.inf,math.inf,11,"heuristic6")
             print("Player 1 performing move ",ai_move,"\n\n")
             curr_board = curr_board.perform_move(ai_move)
         else:
-            human_move = int(input("Choose a move: "))
-            print("Player 2 performing move ",human_move,"\n\n")
-            curr_board = curr_board.perform_move(human_move)
+            legal_moves = curr_board.get_legal_moves()
+            while True:
+                human_move = int(input("Choose a move: "))
+                if human_move in legal_moves:
+                    print("Player 2 performing move ",human_move,"\n\n")
+                    curr_board = curr_board.perform_move(human_move)
+                    break
+                else:
+                    print("Illegal Move\n\n")
+        print(curr_board)
+    print(curr_board)
+    player1_score = curr_board.player_stores[0]
+    player2_score = curr_board.player_stores[1]
+    if player1_score>player2_score:
+        print("Player 1 Wins!!!\n\n")
+    elif player2_score>player1_score:
+        print("Player 2 Wins!!!\n\n")
+    else:
+        print("Tie!!!\n\n")
 elif choice==2:
     curr_board = BoardState()
+    print(curr_board)
     while not curr_board.is_terminal():
-        print(curr_board)
         if curr_board.player==1:
-            val, ai_move = alpha_beta_decision(curr_board,-math.inf,math.inf,11,"heuristic3")
+            val, ai_move = alpha_beta_decision(curr_board,-math.inf,math.inf,11,"heuristic6")
             print("Player 2 performing move ",ai_move,"\n\n")
             curr_board = curr_board.perform_move(ai_move)
         else:
-            human_move = int(input("Choose a move: "))
-            print("Player 1 performing move ",human_move,"\n\n")
-            curr_board = curr_board.perform_move(human_move)
+            legal_moves = curr_board.get_legal_moves()
+            while True:
+                human_move = int(input("Choose a move: "))
+                if human_move in legal_moves:
+                    print("Player 1 performing move ",human_move,"\n\n")
+                    curr_board = curr_board.perform_move(human_move)
+                    break
+                else:
+                    print("Illegal Move")
+        print(curr_board)
+    print(curr_board)
+    player1_score = curr_board.player_stores[0]
+    player2_score = curr_board.player_stores[1]
+    if player1_score>player2_score:
+        print("Player 1 Wins!!!\n\n")
+    elif player2_score>player1_score:
+        print("Player 2 Wins!!!\n\n")
+    else:
+        print("Tie!!!\n\n")
 elif choice==3:
-    pass
+    curr_board = BoardState()
 else:
     print("Invalid Mode")
 
